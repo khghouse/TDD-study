aa8ff962b81dd3fe6d19a8c40715ec1130b57198
khghouse <khghoue@gmail.com>
2021-02-17 20:18:57 +0900

    ch01 p45

---
+++ src/main/java/com/study/ch01/Account.java
@@ -0,0 +1,5 @@
+package com.study.ch01;
+
+public class Account {
+
+}
---
+++ src/test/java/com/study/ch01/AccountTest.java
@@ -0,0 +1,22 @@
+package com.study.ch01;
+
+public class AccountTest {
+
+	public void testAccount() throws Exception {
+		Account account = new Account();
+		if (account == null) {
+			throw new Exception("계좌생성실패");
+		}
+	}
+
+	public static void main(String[] args) {
+		AccountTest test = new AccountTest();
+		try {
+			test.testAccount();
+		} catch (Exception e) {
+			System.out.println("실패(X)");
+			return;
+		}
+		System.out.println("성공(O)");
+	}
+}
