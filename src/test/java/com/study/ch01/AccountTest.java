8ed70040c27741ba250bc067d96527808f14f353
khghouse <khghoue@gmail.com>
2021-02-18 20:40:27 +0900

    ch01 p88

--- src/test/java/com/study/ch01/AccountTest.java
+++ src/test/java/com/study/ch01/AccountTest.java
@@ -1,22 +1,43 @@
 package com.study.ch01;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class AccountTest {
 
-	public void testAccount() throws Exception {
-		Account account = new Account();
-		if (account == null) {
-			throw new Exception("계좌생성실패");
-		}
+	private Account account;
+
+	@Before
+	public void setup() {
+		account = new Account(10000);
 	}
 
-	public static void main(String[] args) {
-		AccountTest test = new AccountTest();
-		try {
-			test.testAccount();
-		} catch (Exception e) {
-			System.out.println("실패(X)");
-			return;
-		}
-		System.out.println("성공(O)");
+	@Test
+	public void testAccount() throws Exception {}
+
+	@Test
+	public void testGetBalance() throws Exception {
+		assertEquals("10000원으로 계좌 생성 후 잔고 조회", 10000, account.getBalance());
+
+		account = new Account(1000);
+		assertEquals(1000, account.getBalance());
+
+		account = new Account(0);
+		assertEquals(0, account.getBalance());
+	}
+
+	@Test
+	public void testDeposit() throws Exception {
+		account.deposit(1000);
+		assertEquals(11000, account.getBalance());
+	}
+
+	@Test
+	public void testWithdraw() throws Exception {
+		account.withdraw(1000);
+		assertEquals(9000, account.getBalance());
 	}
 }
